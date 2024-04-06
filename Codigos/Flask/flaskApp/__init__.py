35049cfc13d809bb0789b209092eb4b8cfc5e346
MiguelExtremo <mec1006@alu.ubu.es>
2024-04-06 17:56:31 +0200

    Backend

--- Codigos/Flask/flaskApp/__init__.py
+++ Codigos/Flask/flaskApp/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     
     # Importar y registrar los Blueprints o vistas
     from .routes import main as main_routes
