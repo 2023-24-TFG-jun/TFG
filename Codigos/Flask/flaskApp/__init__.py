d5097481f5484fb31361535ce02df9fc5535e6a7
MiguelExtremo <mec1006@alu.ubu.es>
2024-05-13 16:14:29 +0200

    Prueba en render

--- Codigos/Flask/flaskApp/__init__.py
+++ Codigos/Flask/flaskApp/__init__.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 def create_app():
     app = Flask(__name__)
     CORS(app, supports_credentials=True)
-    app.config['UPLOAD_FOLDER'] = 'Codigos/Flask/uploads'
+    app.config['UPLOAD_FOLDER'] = 'uploads'
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
  
     from .routes import main as main_routes
